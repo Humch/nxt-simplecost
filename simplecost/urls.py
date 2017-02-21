5ec34051fa1227073f32c7387c4ae9b5b72a536f
Fabien Schlegel <fabienschlegel@yahoo.fr>
2017-02-21 13:40:26 +0100

    fix expense create response

--- simplecost/urls.py
+++ simplecost/urls.py
@@ -1,8 +1,10 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
+from auxiliare import urls
 
 from .views import ExpenseListView, ExpenseCreate, print_it
 
 urlpatterns = [
+    url(r'^', include('auxiliare.urls')),
     url(r'^list-expense/$', ExpenseListView.as_view(), name = 'expense-list' ),
     url(r'^create-expense/$', ExpenseCreate.as_view(), name='expense-create'),
     url(r'^print-expense/$', print_it, name = 'print-expense-list' ),
